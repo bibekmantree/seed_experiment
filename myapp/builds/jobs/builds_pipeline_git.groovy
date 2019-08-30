63861fdf1ac3d2005192e22fb27d285d2fa3ed5e
bibekmantree <badbond420@gmail.com>
2019-08-30 11:19:34 +0000

    Adds pipelie that takes from git (basic)

---
+++ myapp/builds/jobs/builds_pipeline_git.groovy
@@ -0,0 +1,20 @@
+pipelineJob('myapp_git_pipeline') {
+    logRotator {
+        numToKeep(7)
+    }
+    properties {
+        githubProjectUrl('https://github.com/bibekmantree/azure_terraform_jenkins.git')
+    }
+    triggers {
+        githubPush()
+        //scm('H/4 * * * *')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/bibekmantree/azure_terraform_jenkins.git')
+            }
+        }
+    }
+    
+}
