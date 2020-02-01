76576ea5981f059e24771ec06ec73a69521c89de
Bibek <badbond420@gmail.com>
2020-02-01 08:50:27 -0800

    Update injob_pipeline.groovy

--- myapp/builds/pipelines/injob_pipeline.groovy
+++ myapp/builds/pipelines/injob_pipeline.groovy
@@ -6,31 +6,6 @@ pipeline {
                 sh 'ls -al'
              }
         }
-
-        stage('Set Terraform path') {
-            steps {
-                script {
-                    def tfHome = tool name: 'Terraform'
-                    env.PATH = "${tfHome}:${env.PATH}"
-                }
-                sh 'terraform --version'
-            }
-        }
-
-        stage('terraform init') {
-           steps {
-
-               sh 'terraform init '
-           }
-        }
-        /*
-        stage('terraform plan') {
-           steps {
-
-               sh 'terraform plan '
-           }
-        }
-        */
     }
 
 }
