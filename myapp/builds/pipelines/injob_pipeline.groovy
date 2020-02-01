927d55ab37e939fc050e04c153d5711e43238719
Bibek <badbond420@gmail.com>
2020-02-01 09:49:25 -0800

    Update injob_pipeline.groovy

--- myapp/builds/pipelines/injob_pipeline.groovy
+++ myapp/builds/pipelines/injob_pipeline.groovy
@@ -1,11 +1,12 @@
 pipeline {
     agent any
     stages {
-        stage('initial_step') {
-            steps {
-                sh 'ls -al'
-             }
-        }
+        stage('SCM') {
+            steps { 
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PathRestriction', excludedRegions: '', includedRegions: '''myapp/.*
+                ''']], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/bibekmantree/delete_dsl_mono.git']]])
+            }
+        }//end SCM
     }
 
 }
