4f6c3466b1d0709c6dcaafeb85606ae8808271a0
Bibek <badbond420@gmail.com>
2020-02-04 02:26:38 -0800

    Update injob_pipeline_2.groovy

--- myapp/builds/pipelines/injob_pipeline_2.groovy
+++ myapp/builds/pipelines/injob_pipeline_2.groovy
@@ -7,6 +7,12 @@ pipeline {
                 ''']], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/bibekmantree/delete_dsl_mono.git']]])
             }
         }//end SCM
+        
+         /*stage ('Cleanup') {
+            steps {
+                step([$class: 'WsCleanup']) 
+            }
+        } */
     }
 
 }
