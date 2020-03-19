03824d82795accd271f53ac123cfc0e39267d58f
Bibek <badbond420@gmail.com>
2020-03-19 07:40:44 -0400

    Update injob_pipeline.groovy

--- myapp/builds/pipelines/injob_pipeline.groovy
+++ myapp/builds/pipelines/injob_pipeline.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    agent any
+    agent {label 'test'}
     stages {
         stage('Clean Up'){
           steps{
@@ -7,12 +7,26 @@ pipeline {
           }
         }
         stage('SCM') {
+          agent {label 'master'}
             steps { 
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PathRestriction', excludedRegions: '', includedRegions: '''myapp/.*
                 ''']], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/bibekmantree/delete_dsl_mono.git']]])
             }
         }//end SCM
-        
+        stage('SCM_checkout_slave') {
+            steps { 
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PathRestriction', excludedRegions: '', includedRegions: '''myapp/.*
+                ''']], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/bibekmantree/delete_dsl_mono.git']]])
+            }
+        }
+        stage('Shell') {
+            steps {
+              container('test') {
+                sh "ls -la"
+                sh "whoami"
+              }
+            }
+        }
          //stage ('Cleanup') {
            // steps {
              //   step([$class: 'WsCleanup']) 
